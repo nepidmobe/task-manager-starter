45b30dde45a4abca5e200c8b976b28cb735cf904
Dipen Thapa <nepidtube@gmail.com>
2022-04-22 19:07:21 +0545

    done

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://nepidtube:nepid123@cluster0.3xfnw.mongodb.net/task-manager?retryWrites=true&w=majority";
-
 const connectDB = (uri) => {
   return mongoose.connect(uri, {
     useNewUrlParser: true,
